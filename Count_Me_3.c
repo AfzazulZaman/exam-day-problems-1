71a24bace8b4fb3afee3acad4fd32fc769f59e5a
zamanankon <zamanankon3@gmail.com>
2024-10-06 17:14:08 +0600

    done

--- Count_Me_3.c
+++ Count_Me_3.c
@@ -1,36 +1,35 @@
 #include<stdio.h>
-#include<string.h>
 int main(){
       int T;
       scanf("%d",&T);
-       
-       while (T--)
-       {
-         int lower=0,upper=0,digits=0;
-          char S[10001];
-          scanf("%s",S);
 
-          for (int i = 0; S[i]!='\0'; i++)
-          {
-              if (islower(S[i]))
-              {
-                lower++;
-              }
-              if (isupper(S[i]))
-              {
-                upper++;
-              }
-              if (isdigit(S[i]))
-              {
-                digits++;
-              }
-              
-             
-              
+     while (T--)
+     {
+   
+       char S[10001];
+       scanf("%s",S);
+           int digit=0,small=0,upper=0;
+       for (int i = 0; S[i]!='\0'; i++)
+       {
+           if (S[i]>='a' && S[i]<='z')
+           {
+              small++;
+           }
+           if (S[i]>='A' && S[i]<='Z')
+           {
+              upper++;
+           }
+           if (S[i]>='0' && S[i]<='9')
+           {
+             digit++;
+           }
+               
+           
           }
-           printf("%d %d %d\n",upper,lower,digits);
-          
-       }
-       
+  
+         printf("%d %d %d\n",upper,small,digit);
+            }
+    
+      
     return 0;
 }
